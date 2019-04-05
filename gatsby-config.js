138c534c6e7fa6cb4e43db8bf6c5d35e80911917
yonatanmk <yonatanmk@gmail.com>
2019-04-05 15:11:41 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,15 +20,15 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `/20-min-pwa-tutorial`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/pwa20.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
